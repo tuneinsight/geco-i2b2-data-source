0b99f41dc7a630b742c7e74be5ef7543e85b3a6f
Mickaël Misbach <mickael@misba.ch>
2021-12-09 18:23:35 +0100

    add explore query implementation

---
+++ pkg/i2b2datasource/explore_query.go
@@ -0,0 +1,97 @@
+package i2b2datasource
+
+import (
+	"fmt"
+	"strconv"
+
+	i2b2apimodels "github.com/ldsec/geco-i2b2-data-source/pkg/i2b2api/models"
+	"github.com/ldsec/geco-i2b2-data-source/pkg/i2b2datasource/models"
+)
+
+// ExploreQuery makes an explore query, i.e. two i2b2 CRC queries, a PSM and a PDO query.
+func (ds I2b2DataSource) ExploreQuery(params *models.ExploreQueryParameters) (patientCount uint64, patientList []uint64, err error) {
+
+	if i2b2PatientCount, i2b2PatientSetID, err := ds.doExploreQuery(params); err != nil {
+		return 0, nil, err
+	} else if i2b2PatientIDs, err := ds.getPatientIDs(i2b2PatientSetID); err != nil {
+		return 0, nil, err
+	} else if patientCount, err = strconv.ParseUint(i2b2PatientCount, 10, 64); err != nil {
+		return 0, nil, fmt.Errorf("parsing patient count: %v", err)
+	} else {
+		for _, patientID := range i2b2PatientIDs {
+			if parsedPatientID, err := strconv.ParseUint(patientID, 10, 64); err != nil {
+				return 0, nil, fmt.Errorf("parsing patient ID: %v", err)
+			} else {
+				patientList = append(patientList, parsedPatientID)
+			}
+		}
+	}
+
+	return
+}
+
+// doExploreQuery requests the explore query to the i2b2 CRC and parse its results.
+func (ds I2b2DataSource) doExploreQuery(params *models.ExploreQueryParameters) (patientCount, patientSetID string, err error) {
+
+	// build query
+	panels, timing := params.Definition.ToI2b2APIModel()
+	psmReq := i2b2apimodels.NewCrcPsmReqFromQueryDef(
+		ds.i2b2Client.Ci,
+		params.Id,
+		panels,
+		timing,
+		[]i2b2apimodels.ResultOutputName{i2b2apimodels.ResultOutputPatientSet, i2b2apimodels.ResultOutputCount},
+	)
+
+	// request query
+	var psmResp *i2b2apimodels.CrcPsmRespMessageBody
+	if psmResp, err = ds.i2b2Client.CrcPsmReqFromQueryDef(&psmReq); err != nil {
+		return "", "", fmt.Errorf("requesting PSM query: %v", err)
+	}
+
+	// extract results from result instances
+	for i, qri := range psmResp.Response.QueryResultInstances {
+
+		// check status
+		if err := qri.CheckStatus(); err != nil {
+			return "", "", fmt.Errorf("found error in query result instance %v: %v", i, err)
+		}
+
+		// extract result
+		switch qri.QueryResultType.Name {
+		case string(i2b2apimodels.ResultOutputPatientSet):
+			if patientSetID != "" {
+				ds.logger.Warnf("unexpected additional patient set result in i2b2 CRC PSM response (previous: %v)", patientSetID)
+			}
+			patientSetID = qri.ResultInstanceID
+
+		case string(i2b2apimodels.ResultOutputCount):
+			if patientCount != "" {
+				ds.logger.Warnf("unexpected additional patient count result in i2b2 CRC PSM response (previous: %v)", patientCount)
+			}
+			patientCount = qri.SetSize
+
+		default:
+			ds.logger.Warnf("unexpected result in i2b2 CRC PSM response: %v", qri.QueryResultType.Name)
+		}
+	}
+
+	if patientCount == "" || patientSetID == "" {
+		return "", "", fmt.Errorf("missing result from i2b2 PSM response: patientCount=%v, patientSetID=%v", patientCount, patientSetID)
+	}
+	return
+}
+
+// getPatientIDs retrieves the list of patient IDs from the i2b2 CRC using a patient set ID.
+func (ds I2b2DataSource) getPatientIDs(patientSetID string) (patientIDs []string, err error) {
+	pdoReq := i2b2apimodels.NewCrcPdoReqFromInputList(patientSetID)
+	var pdoResp *i2b2apimodels.CrcPdoRespMessageBody
+	if pdoResp, err = ds.i2b2Client.CrcPdoReqFromInputList(&pdoReq); err != nil {
+		return nil, fmt.Errorf("requesting PDO query: %v", err)
+	}
+
+	for _, patient := range pdoResp.Response.PatientData.PatientSet.Patient {
+		patientIDs = append(patientIDs, patient.PatientID)
+	}
+	return
+}
---
+++ pkg/i2b2datasource/explore_query_test.go
@@ -0,0 +1,285 @@
+package i2b2datasource
+
+import (
+	"testing"
+
+	"github.com/ldsec/geco-i2b2-data-source/pkg/i2b2datasource/models"
+	"github.com/stretchr/testify/require"
+)
+
+func TestExploreQueryConcept(t *testing.T) {
+	ds := GetTestDataSource(t)
+
+	count, patientList, err := ds.ExploreQuery(&models.ExploreQueryParameters{
+		Id:         "0",
+		Definition: models.ExploreQueryDefinition{
+			Panels: []models.Panel{{
+				Not:          false,
+				ConceptItems: []models.ConceptItem{{
+					QueryTerm: "/TEST/test/1/",
+				}},
+			}},
+		},
+	})
+	require.NoError(t, err)
+	t.Logf("results: count=%v, patientList=%v", count, patientList)
+	require.EqualValues(t, 3, count)
+	require.Subset(t, []uint64{1, 2, 3}, patientList)
+
+	count, patientList, err = ds.ExploreQuery(&models.ExploreQueryParameters{
+		Id:         "1",
+		Definition: models.ExploreQueryDefinition{
+			Panels: []models.Panel{{
+				Not:          false,
+				ConceptItems: []models.ConceptItem{{
+					QueryTerm: "/TEST/test/3/",
+				}},
+			}},
+		},
+	})
+	require.NoError(t, err)
+	t.Logf("results: count=%v, patientList=%v", count, patientList)
+	require.EqualValues(t, 4, count)
+	require.Subset(t, []uint64{1, 2, 3, 4}, patientList)
+
+	count, patientList, err = ds.ExploreQuery(&models.ExploreQueryParameters{
+		Id:         "1",
+		Definition: models.ExploreQueryDefinition{
+			Panels: []models.Panel{{
+				Not:          false,
+				ConceptItems: []models.ConceptItem{{
+					QueryTerm: "/TEST/test/1/",
+				}},
+			},{
+				Not:          false,
+				ConceptItems: []models.ConceptItem{{
+					QueryTerm: "/TEST/test/2/",
+				}},
+			},{
+				Not:          false,
+				ConceptItems: []models.ConceptItem{{
+					QueryTerm: "/TEST/test/3/",
+				}},
+			}},
+		},
+	})
+	require.NoError(t, err)
+	t.Logf("results: count=%v, patientList=%v", count, patientList)
+	require.EqualValues(t, 3, count)
+	require.Subset(t, []uint64{1, 2, 3}, patientList)
+
+	count, patientList, err = ds.ExploreQuery(&models.ExploreQueryParameters{
+		Id:         "1",
+		Definition: models.ExploreQueryDefinition{
+			Panels: []models.Panel{{
+				Not:          false,
+				ConceptItems: []models.ConceptItem{{
+					QueryTerm: "/TEST/test/1/",
+				},{
+					QueryTerm: "/TEST/test/3/",
+				}},
+			}},
+		},
+	})
+	require.NoError(t, err)
+	t.Logf("results: count=%v, patientList=%v", count, patientList)
+	require.EqualValues(t, 4, count)
+	require.Subset(t, []uint64{1, 2, 3, 4}, patientList)
+}
+
+func TestExploreQueryConceptValue(t *testing.T) {
+	ds := GetTestDataSource(t)
+
+	count, patientList, err := ds.ExploreQuery(&models.ExploreQueryParameters{
+		Id:         "0",
+		Definition: models.ExploreQueryDefinition{
+			Panels: []models.Panel{{
+				Not:          false,
+				ConceptItems: []models.ConceptItem{{
+					QueryTerm: "/TEST/test/1/",
+					Operator:  "EQ",
+					Value:     "10",
+					Type:      "NUMBER",
+				}},
+			}},
+		},
+	})
+	require.NoError(t, err)
+	t.Logf("results: count=%v, patientList=%v", count, patientList)
+	require.EqualValues(t, 1, count)
+	require.Subset(t, []uint64{1}, patientList)
+
+	count, patientList, err = ds.ExploreQuery(&models.ExploreQueryParameters{
+		Id:         "1",
+		Definition: models.ExploreQueryDefinition{
+			Panels: []models.Panel{{
+				Not:          false,
+				ConceptItems: []models.ConceptItem{{
+					QueryTerm: "/TEST/test/1/",
+					Operator:  "EQ",
+					Value:     "10",
+					Type:      "NUMBER",
+				},{
+					QueryTerm: "/TEST/test/3/",
+					Operator:  "EQ",
+					Value:     "20",
+					Type:      "NUMBER",
+				}},
+			}},
+		},
+	})
+	require.NoError(t, err)
+	t.Logf("results: count=%v, patientList=%v", count, patientList)
+	require.EqualValues(t, 2, count)
+	require.Subset(t, []uint64{1, 4}, patientList)
+
+	// todo: maybe it's because of modifier!!!
+	count, patientList, err = ds.ExploreQuery(&models.ExploreQueryParameters{
+		Id:         "1",
+		Definition: models.ExploreQueryDefinition{
+			Panels: []models.Panel{{
+				Not:          false,
+				ConceptItems: []models.ConceptItem{{
+					QueryTerm: "/TEST/test/3/",
+					Operator:  "LIKE[contains]",
+					Value:     "c",
+					Type:      "TEXT",
+				}},
+			}},
+		},
+	})
+	require.NoError(t, err)
+	t.Logf("results: count=%v, patientList=%v", count, patientList)
+	require.EqualValues(t, 2, count)
+	require.Subset(t, []uint64{1, 4}, patientList)
+}
+
+func TestExploreQueryModifier(t *testing.T) {
+	ds := GetTestDataSource(t)
+
+	count, patientList, err := ds.ExploreQuery(&models.ExploreQueryParameters{
+		Id:         "0",
+		Definition: models.ExploreQueryDefinition{
+			Panels: []models.Panel{{
+				Not:          false,
+				ConceptItems: []models.ConceptItem{{
+					QueryTerm: "/TEST/test/1/",
+					Modifier: struct {
+						Key         string
+						AppliedPath string
+					}{
+						Key:         "/TEST/modifiers/1/",
+						AppliedPath: "/test/1/",
+					},
+				}},
+			}},
+		},
+	})
+	require.NoError(t, err)
+	t.Logf("results: count=%v, patientList=%v", count, patientList)
+	require.EqualValues(t, 2, count)
+	require.Subset(t, []uint64{1, 3}, patientList)
+
+	count, patientList, err = ds.ExploreQuery(&models.ExploreQueryParameters{
+		Id:         "1",
+		Definition: models.ExploreQueryDefinition{
+			Panels: []models.Panel{{
+				Not:          false,
+				ConceptItems: []models.ConceptItem{{
+					QueryTerm: "/TEST/test/1/",
+					Modifier: struct {
+						Key         string
+						AppliedPath string
+					}{
+						Key:         "/TEST/modifiers/1/",
+						AppliedPath: "/test/1/",
+					},
+				},{
+					QueryTerm: "/TEST/test/2/",
+					Modifier: struct {
+						Key         string
+						AppliedPath string
+					}{
+						Key:         "/TEST/modifiers/2text/",
+						AppliedPath: "/test/2/",
+					},
+				}},
+			}},
+		},
+	})
+	require.NoError(t, err)
+	t.Logf("results: count=%v, patientList=%v", count, patientList)
+	require.EqualValues(t, 3, count)
+	require.Subset(t, []uint64{1, 2, 3}, patientList)
+}
+
+func TestExploreQueryModifierValue(t *testing.T) {
+	ds := GetTestDataSource(t)
+
+	count, patientList, err := ds.ExploreQuery(&models.ExploreQueryParameters{
+		Id:         "0",
+		Definition: models.ExploreQueryDefinition{
+			Panels: []models.Panel{{
+				Not:          false,
+				ConceptItems: []models.ConceptItem{{
+					QueryTerm: "/TEST/test/2/",
+					Operator:  "LIKE[contains]",
+					Value:     "cd",
+					Type:      "TEXT",
+					Modifier: struct {
+						Key         string
+						AppliedPath string
+					}{
+						Key:         "/TEST/modifiers/2text/",
+						AppliedPath: "/test/2/",
+					},
+				}},
+			}},
+		},
+	})
+	require.NoError(t, err)
+	t.Logf("results: count=%v, patientList=%v", count, patientList)
+	require.EqualValues(t, 1, count)
+	require.Subset(t, []uint64{1}, patientList)
+
+	count, patientList, err = ds.ExploreQuery(&models.ExploreQueryParameters{
+		Id:         "1",
+		Definition: models.ExploreQueryDefinition{
+			Panels: []models.Panel{{
+				Not:          false,
+				ConceptItems: []models.ConceptItem{{
+					QueryTerm: "/TEST/test/3/",
+					Operator:  "LIKE[exact]",
+					Value:     "def",
+					Type:      "TEXT",
+					Modifier: struct {
+						Key         string
+						AppliedPath string
+					}{
+						Key:         "/TEST/modifiers/3text/",
+						AppliedPath: "/test/3/",
+					},
+				}},
+			},{
+				Not:          false,
+				ConceptItems: []models.ConceptItem{{
+					QueryTerm: "/TEST/test/2/",
+					Operator:  "LIKE[begin]",
+					Value:     "a",
+					Type:      "TEXT",
+					Modifier: struct {
+						Key         string
+						AppliedPath string
+					}{
+						Key:         "/TEST/modifiers/2text/",
+						AppliedPath: "/test/2/",
+					},
+				}},
+			}},
+		},
+	})
+	require.NoError(t, err)
+	t.Logf("results: count=%v, patientList=%v", count, patientList)
+	require.EqualValues(t, 1, count)
+	require.Subset(t, []uint64{2}, patientList)
+}

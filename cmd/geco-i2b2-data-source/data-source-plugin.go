b43cdc99ac72004b83dc229a2f9bf6b93ae9221f
Mickaël Misbach <mickael@misba.ch>
2021-12-22 17:33:08 +0100

    update of geco version to use and expose DataSourceType

--- cmd/geco-i2b2-data-source/data-source-plugin.go
+++ cmd/geco-i2b2-data-source/data-source-plugin.go
@@ -5,5 +5,8 @@ import (
 	"github.com/ldsec/geco/pkg/sdk"
 )
 
+// DataSourceType is the type of the data source.
+var DataSourceType sdk.DataSourceType = "i2b2-medco"
+
 // DataSourcePluginFactory exports a factory function compatible with GeCo data source plugin SDK.
 var DataSourcePluginFactory sdk.DataSourcePluginFactory = datasource.NewI2b2DataSource
--- internal/plugin_test.go
+++ internal/plugin_test.go
@@ -4,24 +4,16 @@ import (
 	"plugin"
 	"testing"
 
-	"github.com/ldsec/geco/pkg/sdk"
+	gecoconf "github.com/ldsec/geco/pkg/common/configuration"
+	"github.com/ldsec/geco/pkg/datamanager"
+	gecosdk "github.com/ldsec/geco/pkg/sdk"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
 )
 
-func TestPlugin(t *testing.T) {
-	p, err := plugin.Open("../build/geco-i2b2-data-source.so")
-	require.NoError(t, err)
+const pluginPath = "../build/geco-i2b2-data-source.so"
 
-	dsSymbol, err := p.Lookup("DataSourcePluginFactory")
-	require.NoError(t, err)
-
-	pluginFactory, ok := dsSymbol.(*sdk.DataSourcePluginFactory)
-	t.Logf("%T", pluginFactory)
-	t.Logf("%T", dsSymbol)
-
-	require.True(t, ok)
-
+func getTestConfig() map[string]string {
 	config := make(map[string]string)
 	config["i2b2.api.url"] = "http://localhost:8080/i2b2/services"
 	config["i2b2.api.domain"] = "i2b2demo"
@@ -37,6 +29,29 @@ func TestPlugin(t *testing.T) {
 	config["db.schema-name"] = "gecodatasourceplugintest"
 	config["db.user"] = "postgres"
 	config["db.password"] = "postgres"
+	return config
+}
+
+func TestPlugin(t *testing.T) {
+	p, err := plugin.Open(pluginPath)
+	require.NoError(t, err)
+
+	dsTypeSymbol, err := p.Lookup("DataSourceType")
+	require.NoError(t, err)
+
+	dsType, ok := dsTypeSymbol.(*gecosdk.DataSourceType)
+	require.True(t, ok)
+	require.EqualValues(t, "i2b2-medco", *dsType)
+
+	dsSymbol, err := p.Lookup("DataSourcePluginFactory")
+	require.NoError(t, err)
+
+	pluginFactory, ok := dsSymbol.(*gecosdk.DataSourcePluginFactory)
+	t.Logf("%T", pluginFactory)
+	t.Logf("%T", dsSymbol)
+	require.True(t, ok)
+
+	config := getTestConfig()
 
 	logrus.StandardLogger().SetLevel(logrus.DebugLevel)
 	ds, err := (*pluginFactory)(logrus.StandardLogger(), config)
@@ -46,4 +61,24 @@ func TestPlugin(t *testing.T) {
 	res, _, err := ds.Query("testUser", "searchConcept", []byte(params), nil)
 	require.NoError(t, err)
 	t.Logf("result: %v", string(res))
+}
+
+func TestPluginDataManager(t *testing.T) {
+	dm, err := datamanager.NewDataManager(gecoconf.NewTestDataManagerConfig())
+	require.NoError(t, err)
+
+	err = datamanager.LoadDataSourcePlugin(pluginPath)
+	require.NoError(t, err)
+
+	config := getTestConfig()
+	ds, err := dm.NewDataSource("", "i2b2-medco-test", "test", "i2b2-medco", config, false)
+	require.NoError(t, err)
+
+	params := `{"path": "/", "operation": "children"}`
+	res, err := ds.Query("testUser", "searchConcept", []byte(params), nil)
+	require.NoError(t, err)
+	t.Logf("result: %v", string(res))
+
+	dsFound, err := dm.GetDataSource(ds.UniqueID())
+	require.Equal(t, &ds, dsFound)
 }

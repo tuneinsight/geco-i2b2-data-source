529c33eca639cb28eb5d80b5670fb84a5685bb42
Francesco Marino <francesco@tuneinsight.com>
2022-03-08 20:00:40 +0100

    modified columns of i2b2 tables to accomodate longer paths

--- build/package/i2b2/sql/10-i2b2-modifications.sh
+++ build/package/i2b2/sql/10-i2b2-modifications.sh
@@ -5,6 +5,12 @@ source "$(dirname "$0")/common.sh"
 
 psql $PSQL_PARAMS -d "$I2B2_DB_NAME" <<-EOSQL
 
+    -- increase size of modifier_path in the modifier_dimension table
+    ALTER TABLE i2b2demodata.modifier_dimension ALTER COLUMN modifier_path TYPE varchar(2000);
+
+    -- increase size of concept_path in the concept_dimension table
+    ALTER TABLE i2b2demodata.concept_dimension ALTER COLUMN concept_path TYPE varchar(2000);
+
     -- increase size of encounter_num values (too large for type INT)
     ALTER TABLE i2b2demodata.visit_dimension ALTER COLUMN encounter_num TYPE bigint;
     ALTER TABLE i2b2demodata.observation_fact ALTER COLUMN encounter_num TYPE bigint;

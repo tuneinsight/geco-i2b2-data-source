8aeb4886ee01058f8c07e78a8a78d7a6f89699f3
prosticoco <amprost@protonmail.ch>
2022-03-14 16:16:43 +0100

    updated datasource with new sdk version

--- pkg/datasource/database/database.go
+++ pkg/datasource/database/database.go
@@ -2,38 +2,61 @@ package database
 
 import (
 	"database/sql"
+	"database/sql/driver"
 	"fmt"
 
 	// Registering postgres driver
-	_ "github.com/lib/pq"
+	"github.com/lib/pq"
 	"github.com/sirupsen/logrus"
 )
 
 // NewPostgresDatabase initializes a connection to a postgres database, and loads its structure if not present.
-func NewPostgresDatabase(logger logrus.FieldLogger, host, port, databaseName, schemaName, userLogin, userPassword string) (db *PostgresDatabase, err error) {
-	connectionString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s search_path=%s sslmode=disable", host, port, userLogin, userPassword, databaseName, schemaName)
-
-	logger.Infof("initializing connection to postgres database %v", host)
-	logger.Debugf("postgres connection string is %v", connectionString)
-
-	dbHandle, err := sql.Open("postgres", connectionString)
-	if err != nil {
-		return nil, fmt.Errorf("initializing connection to postgres database: %v", err)
-	}
-
+func NewPostgresDatabase(logger logrus.FieldLogger, config PostgresDatabaseConfig, dbHandle *sql.DB) (db *PostgresDatabase, err error) {
 	db = &PostgresDatabase{
 		logger: logger,
 		handle: dbHandle,
 	}
+	db.PostgresDatabaseConfig = config
 
-	return db, db.loadDdl(schemaName, userLogin)
+	return db, db.loadDdl(db.Schema, db.User)
 }
 
 // PostgresDatabase wraps the Postgres database of the data source.
 type PostgresDatabase struct {
+	PostgresDatabaseConfig
 	logger logrus.FieldLogger
 
 	handle *sql.DB
+}
+
+// PostgresDatabaseConfig is the config used to connect to the postgres database
+type PostgresDatabaseConfig struct {
+	Host     string
+	Port     string
+	Database string
+	Schema   string
+	User     string
+	Password string
+}
+
+// DriverName returns "postgres"
+func (conf PostgresDatabaseConfig) DriverName() string {
+	return "postgres"
+}
+
+// DataSourceName should return the connection string to the db: postgres example "host=localhost port=5432 user=test password=test dbname=test sslmode=disable"
+func (conf PostgresDatabaseConfig) DataSourceName() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s search_path=%s sslmode=disable", conf.Host, conf.Port, conf.User, conf.Password, conf.Database, conf.Schema)
+}
+
+// Driver Should return the database driver
+func (conf PostgresDatabaseConfig) Driver() driver.Driver {
+	return &pq.Driver{}
+}
+
+// Name should return the name of the connected database
+func (conf PostgresDatabaseConfig) Name() string {
+	return conf.Database
 }
 
 func closeRows(rows *sql.Rows, logger logrus.FieldLogger) {
--- pkg/datasource/database/ddl_test.go
+++ pkg/datasource/database/ddl_test.go
@@ -5,13 +5,30 @@ import (
 
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
+	"github.com/tuneinsight/sdk-datasource/pkg/sdk"
 )
+
+var config = PostgresDatabaseConfig{
+	Host:     "localhost",
+	Port:     "5433",
+	Database: "i2b2",
+	Schema:   TestSchemaName,
+	User:     "postgres",
+	Password: "postgres",
+}
 
 func getDB(t *testing.T) *PostgresDatabase {
 	log := logrus.StandardLogger()
 	log.SetLevel(logrus.DebugLevel)
 
-	db, err := NewPostgresDatabase(log, "localhost", "5433", "i2b2", TestSchemaName, "postgres", "postgres")
+	manager := sdk.NewDBManager(sdk.DBManagerConfig{
+		MaxConnectionAttempts:              3,
+		SleepingTimeBetweenAttemptsSeconds: 2,
+	})
+	mDB, err := manager.NewDatabase(config)
+	require.NoError(t, err)
+
+	db, err := NewPostgresDatabase(log, config, mDB.DB)
 	require.NoError(t, err)
 
 	err = db.TestLoadData()

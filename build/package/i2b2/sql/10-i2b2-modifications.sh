a3423f16b2799c0285ae32e4e0692fc71cb47e70
Francesco Marino <francesco@tuneinsight.com>
2022-09-13 12:22:53 +0200

    rolled back i2b2 db colums types to original values

--- build/package/i2b2/sql/10-i2b2-modifications.sh
+++ build/package/i2b2/sql/10-i2b2-modifications.sh
@@ -9,15 +9,5 @@ psql $PSQL_PARAMS -d "$I2B2_DB_NAME" <<-EOSQL
     ALTER TABLE i2b2demodata.modifier_dimension ALTER COLUMN modifier_path TYPE varchar(2000);
     -- increase size of concept_path in the concept_dimension table
     ALTER TABLE i2b2demodata.concept_dimension ALTER COLUMN concept_path TYPE varchar(2000);
-    -- increase size of concept_cd in the concept_dimension table
-    ALTER TABLE i2b2demodata.concept_dimension ALTER COLUMN concept_cd TYPE varchar(2000);
-
-    -- increase size of encounter_num values (too large for type INT)
-    ALTER TABLE i2b2demodata.visit_dimension ALTER COLUMN encounter_num TYPE bigint;
-    ALTER TABLE i2b2demodata.observation_fact ALTER COLUMN encounter_num TYPE bigint;
-    ALTER TABLE i2b2demodata.encounter_mapping ALTER COLUMN encounter_num TYPE bigint;
-
-    -- change tval_char to type text
-    ALTER TABLE i2b2demodata.observation_fact ALTER COLUMN tval_char TYPE text;
 
 EOSQL
